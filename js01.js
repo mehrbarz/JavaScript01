e04ed890b4bf3b3a5fc0043a4c4a2e9ef86cfd88
mehrbarz <mehrbarzegar@gmail.com>
2021-01-07 06:35:20 +0100

    JS full course

--- js01.js
+++ js01.js
@@ -22,9 +22,32 @@ var singleQuote = '<a href="http://mehrbarz.wordpress.com">link</a>' ;   // no n
 
 var backticks = `'<a href="http://mehrbarz.wordpress.com">link</a>' `  // with backticks it is now possible to use both " and ' 
 
+var firstName = "Mehran";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+var lastName = "Barzegar";
 
+var secondToLastLetterOfLastName = lastName[lastName.length -2];
+
+function wordBlanks(myNoun , myAdjective , myVerb , myAdverb) {
+    var result = ""; 
+    result += "The " +" "+ myAdjective +" "+ myNoun + " " + myVerb + " the grass" + myAdverb ;
+    return result;
+}
+
+var ourArray = ["Jake" , 32];
+
+var myArray = ["que" , 3];
+var myArray01 = [ ["Array" , "nested"] , ["Another" , 32]] ; // nested array 
+
+var myArray02 = [43,2,4];
+var myData = myArray02[0];  // reading array at index 0
+
+console.log(wordBlanks("pony", "small" , "is eating" , " slowly."));
+
+console.log(lastLetterOfFirstName);
+console.log(secondToLastLetterOfLastName);
 
 console.log("Created");
 console.log(quotes);
-
+console.log(myData);
 
