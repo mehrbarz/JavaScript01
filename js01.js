1f17235d63059f560086e15c4295f613307a22c4
mehrbarz <mehrbarzegar@gmail.com>
2021-01-07 04:34:50 +0100

    JS full course

---
+++ js01.js
@@ -0,0 +1,30 @@
+
+
+/**
+ * code by MehrBarz
+ *  
+ */
+
+var Number  = "78"; // in-line comment
+
+Number = 8;
+
+let OurName = "Great" ;   // in the scope only
+
+const pi = 3.14 ;   // constant
+
+var a;
+var b = 5 ; // delaration and assignment
+
+var quotes = "Those who were seen dancing\"quoted from\" ";   //use \ to escape the " charachter
+
+var singleQuote = '<a href="http://mehrbarz.wordpress.com">link</a>' ;   // no need to escape the "
+
+var backticks = `'<a href="http://mehrbarz.wordpress.com">link</a>' `  // with backticks it is now possible to use both " and ' 
+
+
+
+console.log("Created");
+console.log(quotes);
+
+
